0c9d84b3c890fc220078cfe05d3d567c4e1726b4
Kirill <71875620+kirillvelyugo@users.noreply.github.com>
2021-10-15 14:33:06 +0300

    Add files via upload

---
+++ v.3.1/script/generation/generation_answers.js
@@ -0,0 +1,14 @@
+// Generation answer for 1 type
+function type_1_get_answer() {
+
+}
+
+// Generation answer for 2 type
+function type_2_get_answer() {
+
+}
+
+// Generation answer for 3 type
+function type_3_get_answer(length, text, letter) {
+
+}
